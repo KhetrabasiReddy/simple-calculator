694eb4489f02974399b371d9e7a962466a4aa729
KhetrabasiReddy <reddykhetrabasi@gmail.com>
2024-10-15 23:52:43 +0530

    completed the calculator

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,14 @@
 console.log('Connect me on %cLinkedIn\n','color:#0077B5;font-size:2rem;text-shadow:3px 1px black;','https://www.linkedin.com/in/khetrabasi-reddy-b0ba77224/')
 
+const clickSound = new Audio('../assets/mouse-click-sound.mp3');
+clickSound.preload = 'auto'; 
 
-
+document.querySelectorAll('.inputs').forEach((input) => {
+    input.addEventListener('click', () => {
+        if (!clickSound.paused) {
+            clickSound.currentTime = 0;  
+        }
+        clickSound.play(); 
+    });
+});
 
