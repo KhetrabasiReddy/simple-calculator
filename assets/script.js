ccf06eda81a6b50fe63f55b96a9bc53020d6a1b1
Khetrabasi <reddykhetrabasi@gmail.com>
2024-10-16 00:06:49 +0530

    Update script.js
    changed the url of the audio file

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 console.log('Connect me on %cLinkedIn\n','color:#0077B5;font-size:2rem;text-shadow:3px 1px black;','https://www.linkedin.com/in/khetrabasi-reddy-b0ba77224/')
 
-const clickSound = new Audio('../assets/mouse-click-sound.mp3');
+const clickSound = new Audio('./mouse-click-sound.mp3');
 clickSound.preload = 'auto'; 
 
 document.querySelectorAll('.inputs').forEach((input) => {
